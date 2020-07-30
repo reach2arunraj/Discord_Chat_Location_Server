dac78cab36ed34537c6654fd868582bc5f5d8dd2
reach2arunraj <60217918+reach2arunraj@users.noreply.github.com>
2020-07-30 18:34:58 +0530

    Broadcast Event

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const http = require("http")
+const path = require("path")
+const socketio = require("socket.io")
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+
+const publicDirectoryPath = path.join(__dirname, "../public")
+
+app.use(express.static(publicDirectoryPath))
+
+let count = 0;
+
+//server (emit) --> client ( receive ) - countUpdated
+//client (emit) --> server ( receive ) - increment
+
+io.on("connection", (socket) =>{
+    console.log("New WebSocket connection.")
+
+
+    socket.emit("message", "Welcome.!")
+    socket.broadcast.emit("message", "A new user has joined")
+
+    socket.on("sendMessage", (message) => {
+        io.emit("message", message)
+    })
+    socket.on("disconnect", () =>{
+        io.emit("message", "A user has left")
+    })
+
+})
+
+server.listen(process.env.PORT || 3000 , console.log("Server is running..."))
