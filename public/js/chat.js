fa905e69bb1ebb65552ad4a7dfe7b115699d876e
reach2arunraj <60217918+reach2arunraj@users.noreply.github.com>
2020-07-30 19:53:42 +0530

    Event acknowledgement

--- public/js/chat.js
+++ public/js/chat.js
@@ -6,6 +6,28 @@ socket.on("message", (message)=>{
 
 document.querySelector("#message-form").addEventListener("submit", e => {
     e.preventDefault()
-    const message = e.target.elements.message
-    socket.emit("sendMessage", message)
+
+    const message = e.target.elements.message.value
+
+    socket.emit("sendMessage", message, (error) =>{
+        if(error){
+            return console.log(error)
+        }
+        console.log("Message delivered!...")
+    })
+})
+
+document.querySelector("#send-location").addEventListener("click", ()=> {
+    if(!navigator.geolocation){
+        return alert("Geolocation Feature is not supported by your browser.")
+    }
+    navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position)
+        socket.emit("sendLocation", {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        },() =>{
+            console.log("Location Shared")
+        })
+    })
 })
